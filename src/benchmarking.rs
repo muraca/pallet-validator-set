b051ab4d3bc76951e17d77f0847162ebca6c20e2
Matteo Muraca <mmuraca247@gmail.com>
2024-06-11 09:10:41 +0200

    updated dependencies to polkadot-v1.12.0 updated benchmarks to v2

--- src/benchmarking.rs
+++ src/benchmarking.rs
@@ -1,27 +1,34 @@
 #![cfg(feature = "runtime-benchmarks")]
 
 use super::*;
-use frame_benchmarking::v1::{account, benchmarks, BenchmarkError};
+
+#[allow(unused)]
+use crate::Pallet as ValidatorSet;
+use frame_benchmarking::v2::*;
 use frame_support::traits::EnsureOrigin;
+use frame_system::RawOrigin;
 
 const SEED: u32 = 0;
 
-benchmarks! {
-	add_validator {
-		let origin =
-			T::AddRemoveOrigin::try_successful_origin().map_err(|_| BenchmarkError::Weightless)?;
-		let validator: T::ValidatorId = account("validator", 0, SEED);
-	}: _<T::RuntimeOrigin>(origin, validator)
+#[benchmarks]
+mod benchmarks {
+	use super::*;
 
-	remove_validator {
-		let origin =
-			T::AddRemoveOrigin::try_successful_origin().map_err(|_| BenchmarkError::Weightless)?;
+	#[benchmark]
+	fn add_validator() {
 		let validator: T::ValidatorId = account("validator", 0, SEED);
-	}: _<T::RuntimeOrigin>(origin, validator)
 
-	impl_benchmark_test_suite!(
-		ValidatorSet,
-		crate::mock::new_test_ext(),
-		crate::mock::Test,
-	);
+		#[extrinsic_call]
+		add_validator(RawOrigin::Root, validator);
+	}
+
+	#[benchmark]
+	fn remove_validator() {
+		let validator: T::ValidatorId = account("validator", 0, SEED);
+
+		#[extrinsic_call]
+		remove_validator(RawOrigin::Root, validator);
+	}
+
+	impl_benchmark_test_suite!(ValidatorSet, crate::mock::new_test_ext(), crate::mock::Test);
 }
